83174bfe4906ae5eb165907b951231b8241ccc0b
Enmin Lin <enmin.lin@student.kuleuven.be>
2025-12-20 11:04:21 +0100

    submission ver1

---
+++ verify_submission.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import shutil
+
+import r0927480
+
+def test_run():
+    
+    print("Initializing solver...")
+    solver = r0927480.r0927480()
+    print("Running optimize...")
+    solver.optimize("tour1000.csv")
+    print("Done.")
+
+if __name__ == "__main__":
+    test_run()
